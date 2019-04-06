50b4fdd79dc1dd1e10b65f09870e77ffbfab55be
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-04-06 15:47:01 +1100

    added graph representation through adjacency matrix

---
+++ src/graph.py
@@ -0,0 +1,157 @@
+import abc
+import numpy as np
+
+
+class Graph(abc.ABC):
+    """
+    The base class representation of a graph with all the
+    interface methods.
+    """
+    def __init__(self, num_vertices, directed=False):
+        """
+        :param num_vertices: Total number of vertices present in the graph
+        :param directed: True if the graph is directed, False otherwise
+        """
+        self.num_vertices = num_vertices
+        self.directed = directed
+
+    @abc.abstractmethod
+    def add_edge(self, v1, v2, weight):
+        """
+        :param v1: vertex of the edge
+        :param v2: vertex of the edge
+        :param weight: weight of the edge connecting vertices v1 and v2
+        :return:
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_adjacent_vertices(self, v):
+        """
+        :param v: vertex whose adjacent vertices we want to retrieve
+        :return: all the vertices adjacent to the given vertex
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_in_degree(self, v):
+        """
+
+        :param v: vertex whose in-degree we wish to compute
+        :return:  number of edges which are incident on the given vertex
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_edge_weight(self, v1, v2):
+        """
+
+        :param v1: vertex of the edge whose weight we wish to retrieve
+        :param v2: vertex of the edge whose weight we wish to retrieve
+        :return: return the weight of the edge connecting vertex v1 and v2
+        """
+        pass
+
+    @abc.abstractmethod
+    def display(self):
+        """
+        This method displays the graph
+        """
+        pass
+
+
+class AdjacencyMatrixGraph(Graph):
+    """
+    Represents a graph as an adjacency matrix. A cell in the matrix has
+    a value when there exists an edge between the vertex represented by
+    the row and column numbers.
+
+    Weighted graphs can hold values > 1 in the matrix cells. A value of
+    0 in the cell indicates that there is no edge
+    """
+
+    def __init__(self, num_vertices, directed=False):
+        super(AdjacencyMatrixGraph, self).__init__(num_vertices, directed)
+
+        self.matrix = np.zeros((num_vertices, num_vertices))
+
+    def add_edge(self, v1, v2, weight=1):
+
+        # check if the vertices are valid
+        if v1 >= self.num_vertices or v2 >= self.num_vertices or v1 < 0 or v2 < 0:
+            raise ValueError("Vertices %d and %d are out of bounds" % (v1, v2))
+
+        # check if the weight is positive
+        if weight < 1:
+            raise ValueError("An edge cannot have weight < 1")
+
+        # assign the weight to the edge between the specified vertices
+        self.matrix[v1][v2] = weight
+
+        # in case the graph is undirected, we need an edge which goes in the opposite direction as well
+        if not self.directed:
+            self.matrix[v2][v1] = weight
+
+    def get_adjacent_vertices(self, v):
+
+        # check if the vertex is valid
+        if v >= self.num_vertices or v < 0:
+            raise ValueError("Vertex %d is out of bounds" % v)
+
+        adjacent_vertices = []
+
+        for to_vertex in range(self.num_vertices):
+            if self.matrix[v][to_vertex] > 0:
+                adjacent_vertices.append(to_vertex)
+
+        return adjacent_vertices
+
+    def get_in_degree(self, v):
+
+        # check if the vertex is valid
+        if v >= self.num_vertices or v < 0:
+            raise ValueError("Vertex %d is out of bounds" % v)
+
+        in_degree = 0
+
+        for from_vertex in range(self.num_vertices):
+            if self.matrix[from_vertex][v] > 0:
+                in_degree += 1
+
+        return in_degree
+
+    def get_edge_weight(self, v1, v2):
+
+        # check if the vertices are valid
+        if v1 >= self.num_vertices or v2 >= self.num_vertices or v1 < 0 or v2 < 0:
+            raise ValueError("Vertices %d and %d are out of bounds" % (v1, v2))
+
+        return self.matrix[v1][v2]
+
+    def display(self):
+        for from_vertex in range(self.num_vertices):
+            for to_vertex in self.get_adjacent_vertices(from_vertex):
+                print(from_vertex, "-->", to_vertex)
+
+
+g = AdjacencyMatrixGraph(4)
+
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(2, 3)
+
+
+for i in range(4):
+    print("Adjacent to:", i, g.get_adjacent_vertices(i))
+
+# display the in_degree for each vertex in the graph
+for i in range(4):
+    print("In degree: ", i, g.get_in_degree(i))
+
+# display the weight for each edge in the graph
+for i in range(4):
+    for j in g.get_adjacent_vertices(i):
+        print("Edge weight: ", i, " ", j, "weight: ", g.get_edge_weight(i, j))
+
+# display the graph
+g.display()
